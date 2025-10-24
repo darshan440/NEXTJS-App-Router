16cc93d17ca049e09dae8deccdfdc9d4170186f6
Darshan <darshanthakkar782@gmail.com>
2025-10-24 14:04:18 +0530

    Add initial dashboard, customers, and invoices pages with layout and styles

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,14 @@
+import SideNav from "../ui/dashboard/sidenav";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex h-screen  w-screen flex-col md:flex-row md:overflow-hidden">
+      <div className="w-full flex-none bg-black md:w-64 ">
+        <SideNav />
+      </div>
+      <div className="flex-grow p-6 bg-yellow-200   md:overflow-hidden-y-layout md:p-12 ">
+        {children}
+      </div>
+    </div>
+  );
+}
